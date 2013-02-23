d40ba91974e4d37e21f9586481fcf8c7daafcc6c
Asif Momin <asifmom@thoughtworks.com>
2013-02-23 13:48:58 +0530

    added validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates_presence_of :title, :price, :image_url
+  validates_uniqueness_of :title
+  validates :price,:numericality => {:greater_than_or_equal_to => 0.01,:message => "must be greater than or equal to 0.01"}
+  validates :image_url,:format => {:with => %r{\.(jpg|png|gif)$}i,:message => 'must be valid image with extension jpg, png of gif image'}
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,55 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "Basic Product Validations" do
+    product=Product.new
+    assert product.invalid?
+    assert product.errors[:title]
+    assert product.errors[:image_url]
+    assert product.errors[:price]
+  end
+
+
+  test "Price of Product must be positive" do
+    product = Product.new(:title => 'some title', :description => "some description", :image_url => "a.jpg")
+    product.price=-1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join("")
+
+    product.price=0
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join("")
+
+    product.price=1
+    assert product.valid?
+
+  end
+
+  test "image url" do
+    product = Product.new(:title => 'some title', :description => "some description", :price => 1)
+
+    good=%w{fred.gif a.jpg b.png images/i.jpg}
+
+    bad=%w{a.more images/gif.and}
+
+    good.each do |good_image|
+      product.image_url=good_image
+      assert product.valid?
+    end
+
+
+    bad.each do |bad_image_name|
+      product.image_url=bad_image_name
+      assert product.invalid?
+    end
+
+
+  end
+
+  test "unique title" do
+    product = Product.new(:title => products(:ruby).title, :description => "some description", :price => 1)
+    assert !product.save
+    assert product.invalid?
+  end
 end
