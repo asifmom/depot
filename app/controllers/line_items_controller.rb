a0be866bd39ae7f04ffe84f60b81d361fe00ea5a
Asif Momin <asifmom@thoughtworks.com>
2013-03-03 19:45:40 +0530

    showing the shopping cart as sidebar

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,7 +4,7 @@ class LineItemsController<ApplicationController
     @cart=current_cart
     line_item = @cart.add_product params[:product_id]
     line_item.save
-    redirect_to cart_path(@cart.id),:notice => "line item for product #{product.title}created"
+    redirect_to store_url,:notice => "line item for product #{product.title}created"
   end
 
   def destroy
